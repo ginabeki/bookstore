a4a9de8dceaea5c65b1a66855f24367d595c1ee2
ginabeki <ginabeki3@gmail.com>
2022-12-15 22:24:46 +0300

    Add api

--- src/Components/Pages/Books.js
+++ src/Components/Pages/Books.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Form from './Form';
 import Book from './Book';
+import { getBooks } from '../../redux/books/books';
 
 function Books() {
-  const books = useSelector((state) => state.book);
+  const { books, status } = useSelector((state) => state.book);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!status) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div>
       <div>
         {books.map((book) => (
           <Book
             title={book.title}
-            id={book.id}
+            id={book.item_id}
             author={book.author}
-            key={book.id}
+            key={book.item_id}
           />
         ))}
       </div>
